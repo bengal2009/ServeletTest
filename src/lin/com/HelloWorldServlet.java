8fa7ab72abc06cfa56ca53983e6f5a3cba44594c
bengal2009 <bennylin@outlook.com>
2015-04-21 15:49:58 +0800

    Initial commit

---
+++ src/lin/com/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package lin.com;
+
+import java.io.IOException;
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+public class HelloWorldServlet extends HttpServlet
+{
+    public void doGet (HttpServletRequest req,
+                       HttpServletResponse res)
+            throws ServletException, IOException
+    {
+        PrintWriter out = res.getWriter();
+        out.println("Hello, world");
+        out.close();
+    }
+}
+
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: blin
+  Date: 2015/4/20
+  Time: 下午 05:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>1111</title>
+  </head>
+  <body>
+555577777999測試
+  <%
+    int i;
+    for(i=0;i<10;i++) out.println(i+"<br>");
+   out.flush();
+  %>
+  </body>
+</html>
